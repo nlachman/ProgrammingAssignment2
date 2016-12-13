30cf2d922caf67b301fa70f1a5154078ca3dea07
Nickolaus Lachman <nickolauslachman@gmail.com>
2016-12-12 17:41:50 -0800

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    # Conflicts:
    #	README.md
    #	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a "special" matrix that caches its inverse
+makeCacheMatrix <- function(x = matrix()) { 
+  inv <- NULL                           ## initialize inv as NULL
+  set <- function(y) {                  ## define the set function to assign new 
+    x <<- y                           ## matrix value in parent environment
+    inv <<- NULL}                     ## if there is a new matrix, reset inv to NULL
+  get <- function() x                   ## returns value of the matrix argument
+  
+  setinverse <- function(inverse) inv <<- inverse  ## assigns value in parent environment
+  getinverse <- function() inv                     ## gets the value of inv where called
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
+  ## this allows you to use $ operator in the cacheSolve function
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the matrix returned by makeCacheMatrix function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    return(inv)}
+  sampledata <- x$get()
+  inv <- solve(sampledata, ...)
+  x$setinverse(inv)
+  inv}
